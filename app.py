2f5e132d7da924f81e4427e10f55b992ab0ffa14
Nagaraj021984 <harnath.nag@gmail.com>
2025-06-21 13:38:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+import pytesseract
+
+# Link to Tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Title of the app
+st.title("📷 Math Problem OCR")
+
+# File uploader for image input
+uploaded_image = st.file_uploader("Upload an image of a math problem", type=["jpg", "jpeg", "png"])
+
+# If an image is uploaded
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    st.image(img, caption="🖼 Uploaded Image", use_column_width=True)
+
+    with st.spinner("🔍 Extracting text..."):
+        text = pytesseract.image_to_string(img, config='--psm 6')
+        st.subheader("📝 Extracted Math Problem")
+        st.code(text)
